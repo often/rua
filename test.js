8b3a9bebcd6dd2359c7c7ee2fa801ba947e0172e
Sany <44934721+often@users.noreply.github.com>
2020-06-01 19:07:16 -0600

    add module test

---
+++ test.js
@@ -0,0 +1,6 @@
+import rua from './mod.js' // import the rua module
+
+console.log(rua()) // will return a random user-agent
+console.log(rua('desktop')) // will return a random destop user-agent
+console.log(rua('mobile')) // will return a random mobile user-agent
+console.log(rua('crawler')) // will return a random crawler user-agent
